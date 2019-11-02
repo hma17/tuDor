229e0aca1878ff7235fe10a7de4d7264d811d9a3
hma17 <hma17@duke.edu>
2019-11-02 17:57:32 -0400

    initial commit

--- app/src/main/java/com/example/tudor/LogInActivity.java
+++ app/src/main/java/com/example/tudor/LogInActivity.java
@@ -14,22 +14,6 @@ import android.view.MenuItem;
 
 public class LogInActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +21,5 @@ public class LogInActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-}\
+}
 
